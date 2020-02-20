42b20466ebb6fb1d3e7a095770ab9350a0fb760a
wade.perrigo <wade.perrigo@nutanix.com>
2020-02-20 03:49:32 -0500

    initial commit version 1.0.13

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = {
+    alertPolicy: require('./lib/alertPolicy.js'),
+    application: require('./lib/application.js'),
+    category: require('./lib/category.js'),
+    chart: require('./lib/chart.js'),
+    cluster: require('./lib/cluster.js'),
+    container: require('./lib/container.js'),
+    dashboard: require('./lib/dashboard.js'),
+    event: require('./lib/event.js'),
+    filter: require('./lib/filter.js'),
+    focus: require('./lib/focus.js'),
+    host: require('./lib/host.js'),
+    idempotence: require('./lib/idempotence.js'),
+    image: require('./lib/image.js'),
+    marketplace: require('./lib/marketplace.js'),
+    networks: require('./lib/networks.js'),
+    project: require('./lib/project.js'),
+    protectionDomain: require('./lib/protectionDomain.js'),
+    remoteSite: require('./lib/remoteSite.js'),
+    role: require('./lib/role.js'),
+    report: require('./lib/report.js'),
+    scenario: require('./lib/scenario.js'),
+    securityPolicy: require('./lib/securityPolicy.js'),
+    securityRule: require('./lib/securityRule.js'),
+    snapshots: require('./lib/snapshots.js'),
+    storagePool: require('./lib/storagePool.js'),
+    tag: require('./lib/tag.js'),
+    task: require('./lib/task.js'),
+    vm: require('./lib/vm.js'),
+    volumeGroup: require('./lib/volumeGroup.js')
+}
